8c5ba034900fdceadc8db9400f73b4e3c2aafa11
robygolee <roberto.colombo@golee.it>
2025-01-12 16:07:54 +0100

    zioporco

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,20 @@ module.exports = {
 			sm: '300px',
 			md: '600px'
 		},
+		fontSize: {
+			xs: '0.75rem',
+			sm: '1.25rem',
+			base: '1.5rem',
+			lg: '1.125rem',
+			xl: '2rem',
+			'2xl': '3rem',
+			'3xl': '1.875rem',
+			'4xl': '2.25rem',
+			'5xl': '3rem',
+			'6xl': '4rem',
+			'7xl': '5rem',
+			'8xl': '6rem'
+		},
 		extend: {
 			fontFamily: {
 				coursive: ['Coursive', 'sans-serif'],
