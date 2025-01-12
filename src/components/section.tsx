4686470c717ee554ceb1a1d3a54effcd2e0fc55a
robygolee <roberto.colombo@golee.it>
2025-01-12 13:50:11 +0100

    fix image

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -11,26 +11,6 @@ export default ({
 	description: any
 	children: any
 }) => {
-	const Image = (
-		<div className="flex h-full w-full content-center justify-center overflow-hidden p-10">
-			<img src={image} alt="Image" className="w-full rounded-2xl object-cover" />
-		</div>
-	)
-	const Texts = (
-		<div className="flex h-full w-full flex-col justify-around gap-4 p-8">
-			<div
-				className={`flex flex-col gap-4 sm:text-center ${swap ? 'md:text-right' : 'md:text-left'}`}
-			>
-				<h2
-					className="text-4xl font-bold text-[#303033]"
-					dangerouslySetInnerHTML={{ __html: title }}
-				></h2>
-				<p className="text-[#303033]">{description}</p>
-			</div>
-			<div className="flex justify-center">{children}</div>
-		</div>
-	)
-
 	return (
 		<div
 			className={`flex h-[700px] bg-gradient-to-b from-[#f3f4e7] sm:grid-cols-1 md:grid-cols-2 ${swap ? 'md:flex-row-reverse' : 'md:flex-row'} sm:flex-col`}
