0f419121ea5c9960556bc993c16d1b66e1686ee7
robygolee <roberto.colombo@golee.it>
2025-01-12 11:49:48 +0100

    copertina - section

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/**/*.{astro,html,js,jsx,ts,tsx}', // Specifica dove Tailwind deve cercare le classi
-  ],
-  theme: {
-    extend: {}, // Personalizzazioni opzionali
-  },
-  plugins: [], // Aggiungi plugin di Tailwind, se necessario
+	content: [
+		'./src/**/*.{astro,html,js,jsx,ts,tsx}' // Specifica dove Tailwind deve cercare le classi
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				coursive: ['Coursive', 'sans-serif'],
+				coursive2: ['Coursive2', 'sans-serif'],
+				codec: ['Codec', 'sans-serif']
+			}
+		} // Personalizzazioni opzionali
+	},
+	plugins: [] // Aggiungi plugin di Tailwind, se necessario
 }
